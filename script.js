e25174d897de65f6065af0e415123f00bbbaa788
[Chukwuma Obasi] <ccobasi8@gmail.com>
2021-07-05 21:47:25 +0100

    Add displayBook function

--- script.js
+++ script.js
@@ -9,3 +9,14 @@ function addBook(newAweSomeBook) {
   myBooks.push(newAweSomeBook);
 }
 
+function displayBook(newAweSomeBook){
+  const list = document.querySelector('#books');
+  const row = document.createElement('div');
+  row.innerHTML = `
+  <h4>${newAweSomeBook.title}</h4>
+  <p>${newAweSomeBook.author}</p>
+  <button class="btn danger">Remove</button>
+
+  list.appendChild(row);
+  `
+}
