98c333717e9fb1e1c5676896994adf581dbbcfa9
[Chukwuma Obasi] <ccobasi8@gmail.com>
2021-07-05 22:07:20 +0100

    Add createAwesomeBook function

--- script.js
+++ script.js
@@ -20,3 +20,38 @@ function displayBook(newAweSomeBook){
   list.appendChild(row);
   `
 }
+
+function createAwesomeBooks(e) {
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+
+  e.preventDefault();
+
+  function validateForm(title, author) {
+    let newAweSomeBook;
+    if (title === '' || author === '' ) {
+      // eslint-disable-next-line no-alert
+      alert('please fill all the form');
+    } else {
+      newAweSomeBook = new awesomeBook(title, author);
+      addBook(newAweSomeBook);
+
+      const bookObj = {
+        title: newAweSomeBook.title,
+        author: newAweSomeBook.author,
+      };
+
+      // Put the object into storage
+      const bookIndex = localStorage.length === 0 ? 0 : localStorage.length;
+      localStorage.setItem(bookIndex.toString(), JSON.stringify(bookObj));
+
+      const form = document.getElementById('book-form');
+      form.reset();
+      displayBook(newAweSomeBook);
+    }
+  }
+  validateForm(title, author);
+}
+
+submit.addEventListener('click', (e) => createBook(e));
+
