c47589d60349be96e118c49b54bfab1c9d96b6d2
[Chukwuma Obasi] <ccobasi8@gmail.com>
2021-07-07 21:31:58 +0100

    Fix eslint error

--- script.js
+++ script.js
@@ -1,27 +1,22 @@
 const submit = document.getElementById('submit');
+const myBook = [];
 
 class Book {
   constructor(title, author) {
-    this.bookTitle = title;
-    this.bookAuthor = author;
-  }
-}
-
-class MyBook {
-  constructor() {
-    this.myBook = [];
+    this.title = title;
+    this.author = author;
   }
 
   addBook(newBook) {
-    myBook.push(newBook);
+    this.myBook.push(newBook);
   }
 
   displayBook(newBook) {
     const list = document.querySelector('#book-list');
     const row = document.createElement('div');
     row.innerHTML = `
-      <h4>${newBook.title}</h4>
-      <h4>${newBook.author}</h4>
+      <h4>${this.newBook.title}</h4>
+      <h4>${this.newBook.author}</h4>
       <button><a href="#" class="btn btn-danger btn-sm remove">Remove</a></button><hr>`;
 
     list.appendChild(row);
@@ -31,54 +26,68 @@ class MyBook {
       .indexOf(newBook.title);
       // update book read state in local storage
     localStorage.setItem(bookIndex.toString(), JSON.stringify(newBook));
-
-    // loads previous added books
-    // eslint-disable-next-line func-names
-    window.onload = function () {
-      for (let i = 0; i < localStorage.length; i += 1) {
-        const retrievedObject = localStorage.getItem(i);
-        const newBookObj = JSON.parse(retrievedObject);
-        createNewBookFromLocalStorage(
-          newBookObj.title,
-          newBookObj.author,
-        );
-      }
-    };
-
-    document.querySelector('#book-list').addEventListener('click', (e) => {
-      if (e.target.classList.contains('remove')) {
-        e.target.parentElement.parentElement.remove();
-      }
-    });
-
-    createBook(e); {
-      const title = document.getElementById('title').value;
-      const author = document.getElementById('author').value;
-      e.preventDefault();
-      validateForm(title, author); {
-        let newBook;
-        if (title === '' || author === '') {
-          // eslint-disable-next-line no-alert
-          alert('please fill all the form');
-        } else {
-          newBook = new Books(title, author);
-          addBook(newBook);
-          const bookObj = {
-            title: newBook.title,
-            author: newBook.author,
-          };
-          // Put the object into storage
-          const bookIndex = localStorage.length === 0 ? 0 : localStorage.length;
-          localStorage.setItem(bookIndex.toString(), JSON.stringify(bookObj));
-          const form = document.getElementById('book-form');
-          form.reset();
-          displayBook(newBook);
-        }
-      }
-      validateForm(title, author);
-    }
-    
   }
+}
+
+
+
+
+
+
+
+
+document.querySelector('#book-list').addEventListener('click', (e) => {
+  if (e.target.classList.contains('remove')) {
+    e.target.parentElement.parentElement.remove();
+  }
+});
+
+function addBook(newBook) {
+  myBook.push(newBook);
+}
+
+function displayBook(newBook) {
+  const list = document.querySelector('#book-list');
+  const row = document.createElement('div');
+  row.innerHTML = `
+      <h4>${this.newBook.title}</h4>
+      <h4>${this.newBook.author}</h4>
+      <button><a href="#" class="btn btn-danger btn-sm remove">Remove</a></button><hr>`;
+
+  list.appendChild(row);
+
+  const bookIndex = myBook
+    .map((book) => book.title)
+    .indexOf(newBook.title);
+  // update book read state in local storage
+  localStorage.setItem(bookIndex.toString(), JSON.stringify(newBook));
+}
+
+function createBook(e) {
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  e.preventDefault();
+  function validateForm(title, author) {
+    let newBook;
+    if (title === '' || author === '') {
+      // eslint-disable-next-line no-alert
+      alert('please fill all the form');
+    } else {
+      newBook = new Book(title, author);
+      addBook(newBook);
+      const bookObj = {
+        title: newBook.title,
+        author: newBook.author,
+      };
+        // Put the object into storage
+      const bookIndex = localStorage.length === 0 ? 0 : localStorage.length;
+      localStorage.setItem(bookIndex.toString(), JSON.stringify(bookObj));
+      const form = document.getElementById('book-form');
+      form.reset();
+      displayBook(newBook);
+    }
+  }
+  validateForm(title, author);
 }
 
 submit.addEventListener('click', (e) => createBook(e));
