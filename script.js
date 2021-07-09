e7ee0763fee3a476f33596a00343bfbb94dc8703
Chike1990 <chibuikeuzoechina@gmail.com>
2021-07-09 18:20:13 +0100

    Fix linters

--- script.js
+++ script.js
@@ -11,10 +11,10 @@ class Book {
 class Store {
   static getBooks() {
     let books;
-    if (localStorage.getItem("books") === null) {
+    if (localStorage.getItem('books') === null) {
       books = [];
     } else {
-      books = JSON.parse(localStorage.getItem("books"));
+      books = JSON.parse(localStorage.getItem('books'));
     }
 
     return books;
@@ -23,11 +23,7 @@ class Store {
   static addBook(book) {
     const books = Store.getBooks();
     books.push(book);
-    localStorage.setItem("books", JSON.stringify(books));
-  }
-
-  setToLocalStorage() {
-    localStorage.setItem("books", JSON.stringify(books));
+    localStorage.setItem('books', JSON.stringify(books));
   }
 
   static removeBook(id) {
@@ -39,7 +35,7 @@ class Store {
       }
     });
 
-    localStorage.setItem("books", JSON.stringify(books));
+    localStorage.setItem('books', JSON.stringify(books));
   }
 }
 
@@ -51,9 +47,9 @@ class MyBook {
   }
 
   static addBookToList(book) {
-    const list = document.querySelector("#book-list");
-    const div = document.createElement("div");
-    div.classList.add("table-row")
+    const list = document.querySelector('#book-list');
+    const div = document.createElement('div');
+    div.classList.add('table-row');
 
     div.innerHTML = `
             <div>
@@ -69,32 +65,32 @@ class MyBook {
   }
 
   static deleteBook(el) {
-    if (el.classList.contains("delete")) {
+    if (el.classList.contains('delete')) {
       el.parentElement.parentElement.remove();
     }
   }
 
   static clearFields() {
-    document.querySelector("#title").value = "";
-    document.querySelector("#author").value = "";
+    document.querySelector('#title').value = '';
+    document.querySelector('#author').value = '';
   }
 }
 
 // Event: Display Books
-document.addEventListener("DOMContentLoaded", MyBook.displayBooks);
+document.addEventListener('DOMContentLoaded', MyBook.displayBooks);
 
 // Event: Add a Book
-document.querySelector("#book-form").addEventListener("submit", (e) => {
+document.querySelector('#book-form').addEventListener('submit', (e) => {
   // Prevent actual submit
   e.preventDefault();
 
   // Get form values
-  const title = document.querySelector("#title").value;
-  const author = document.querySelector("#author").value;
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
 
   // Validate
-  if (title === "" || author === "") {
-    MyBook.alert("Please fill in all fields");
+  if (title === '' || author === '') {
+    MyBook.alert('Please fill in all fields');
   } else {
     // Instatiate book
     const book = new Book(title, author);
@@ -111,7 +107,7 @@ document.querySelector("#book-form").addEventListener("submit", (e) => {
 });
 
 // Event: Remove a Book
-document.querySelector("#book-list").addEventListener("click", (e) => {
+document.querySelector('#book-list').addEventListener('click', (e) => {
   // Remove book from Store
   MyBook.deleteBook(e.target);
 
@@ -119,41 +115,37 @@ document.querySelector("#book-list").addEventListener("click", (e) => {
   Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
 });
 
-const store = new Store([]);
-
-const addBtn = document.querySelector("#submit");
-
 /* global luxon */
 /* eslint no-undef: "error" */
 
 const displayDate = () => {
-  const dateNowFormat = luxon.DateTime.now().toFormat("FF");
-  document.querySelector(".date-now").innerText = dateNowFormat;
+  const dateNowFormat = luxon.DateTime.now().toFormat('FF');
+  document.querySelector('.date-now').innerText = dateNowFormat;
 };
 
-const listBtn = document.querySelector(".list__nav");
-const addBookBtn = document.querySelector(".list__add");
-const contactBtn = document.querySelector(".list__contact");
-const listOption = document.querySelector(".container__list");
-const listAdd = document.querySelector(".container__add");
-const listContact = document.querySelector(".container__contact");
+const listBtn = document.querySelector('.list__nav');
+const addBookBtn = document.querySelector('.list__add');
+const contactBtn = document.querySelector('.list__contact');
+const listOption = document.querySelector('.container__list');
+const listAdd = document.querySelector('.container__add');
+const listContact = document.querySelector('.container__contact');
 
-listBtn.addEventListener("click", () => {
-  listOption.style.display = "block";
-  listAdd.style.display = "none";
-  listContact.style.display = "none";
+listBtn.addEventListener('click', () => {
+  listOption.style.display = 'block';
+  listAdd.style.display = 'none';
+  listContact.style.display = 'none';
 });
 
-addBookBtn.addEventListener("click", () => {
-  listOption.style.display = "none";
-  listAdd.style.display = "flex";
-  listContact.style.display = "none";
+addBookBtn.addEventListener('click', () => {
+  listOption.style.display = 'none';
+  listAdd.style.display = 'flex';
+  listContact.style.display = 'none';
 });
 
-contactBtn.addEventListener("click", () => {
-  listOption.style.display = "none";
-  listAdd.style.display = "none";
-  listContact.style.display = "flex";
+contactBtn.addEventListener('click', () => {
+  listOption.style.display = 'none';
+  listAdd.style.display = 'none';
+  listContact.style.display = 'flex';
 });
 
 setInterval(() => {
